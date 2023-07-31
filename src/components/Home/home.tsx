44f9cce1d14d5b8479039f90eab90fe25d7eb91e
muhammadqamar <muhammadqamar111@gmail.com>
2023-07-31 15:47:31 +0500

    added color

--- src/components/Home/home.tsx
+++ src/components/Home/home.tsx
@@ -1,7 +1,15 @@
 import React from "react";
 
+import Card from "../../utils/Cards";
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <h1 className="">Home page</h1>
+
+      <Card />
+    </div>
+  );
 };
 
 export default Home;
