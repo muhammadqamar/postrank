753f9eb6f5a48eb21289ebd45c08e16a487c5160
muhammadqamar <muhammadqamar111@gmail.com>
2023-08-02 13:13:49 +0500

    Modify typography

--- src/Routers/app2.tsx
+++ src/Routers/app2.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import React from "react";
 import AppRouter from "./appRouter";
 
--- src/utils/Typography/index.tsx
+++ src/utils/Typography/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import { Heading } from "./heading";
 
 export { Heading };
