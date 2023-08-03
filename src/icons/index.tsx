67073bc3492a60bd379a2327af4e9a37dbac2d47
muhammadqamar <muhammadqamar111@gmail.com>
2023-08-03 17:05:12 +0500

    Added Icons

--- src/icons/index.tsx
+++ src/icons/index.tsx
@@ -1,5 +1,7 @@
 import { EyeIcon } from "./eyeIcon";
 import { AddIcon } from "./add-icon";
+import { AddContactIcon } from "./addContact";
+import { BackIcon } from "./back";
 import { CalendarIcon } from "./calendar";
 import { CategoryIcon } from "./category";
 import { ContactIcon } from "./contact";
@@ -8,8 +10,11 @@ import { ExpandIcon } from "./expand";
 import { GoogleIcon } from "./google";
 import { GridIcon } from "./grid";
 import { IncreaseIcon } from "./increase";
+import { LoadingErrorIcon } from "./loadingErrorIcon";
 import { LogoutIcon } from "./logout";
 import { MinusIcon } from "./minus";
+import { NoResultIcon } from "./noResult";
+import { OpenNewIcon } from "./openNew";
 import { SadFaceIcon } from "./sadFace";
 import { SearchIcon } from "./search";
 import { SettingsIcon } from "./settings";
@@ -22,6 +27,8 @@ import { WarningIcon } from "./warning";
 export {
   EyeIcon,
   AddIcon,
+  AddContactIcon,
+  BackIcon,
   CalendarIcon,
   CategoryIcon,
   ContactIcon,
@@ -30,8 +37,11 @@ export {
   GoogleIcon,
   GridIcon,
   IncreaseIcon,
+  LoadingErrorIcon,
   LogoutIcon,
   MinusIcon,
+  NoResultIcon,
+  OpenNewIcon,
   SadFaceIcon,
   SearchIcon,
   SettingsIcon,
