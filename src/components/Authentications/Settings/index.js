5f6aa37ae45073ff8853748ad7a1ee0639ed4978
muhammadqamar <muhammadqamar111@gmail.com>
2023-08-10 17:37:47 +0500

    add settings page

--- src/components/Authentications/Settings/index.js
+++ src/components/Authentications/Settings/index.js
@@ -1,10 +1,14 @@
 import React from "react";
 import AccountDetails from "./accountDetails";
+import Feedback from "./feedback";
+import Supports from "./support";
 
 const Index = () => {
   return (
-    <div>
+    <div className="max-w-[1280px] mx-auto p-8">
       <AccountDetails />
+      <Feedback />
+      <Supports />
     </div>
   );
 };
